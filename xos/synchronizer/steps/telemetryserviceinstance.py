4c12f28c2172b977c6681c21ee5fefd12f1c2c85
Test User <test@null.com>
2018-07-20 10:25:00 +0800

    Fix some bugs

--- xos/synchronizer/steps/telemetryserviceinstance.py
+++ xos/synchronizer/steps/telemetryserviceinstance.py
@@ -29,7 +29,7 @@ class SyncTelemetryServiceInstance(SyncInstanceUsingAnsible):
 
     provides = [TelemetryServiceInstance]
 
-    observes = TtelemetryServiceInstance
+    observes = TelemetryServiceInstance
 
     requested_interval = 0
 
